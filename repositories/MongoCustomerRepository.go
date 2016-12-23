b59b330ffd9f9d3e4c0861bd2651f23e592a8c4a
Lehman Black <lblack@curtmfg.com>
2016-12-23 08:32:04 -0600

    Fix imports and bad function signature

--- repositories/MongoCustomerRepository.go
+++ repositories/MongoCustomerRepository.go
@@ -1,9 +1,13 @@
 package repositories
 
+import (
+	"github.com/WileESpaghetti/curt-db-utils/models"
+)
+
 type MongoCustomerRepository struct {
 }
 
-func (repo CustomerRepository) getByApiKey(apiKey) models.Customer {
+func (repo MongoCustomerRepository) getByApiKey(apiKey) models.Customer {
 	// FIXME not implemented
 	return nil
 }
