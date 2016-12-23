370b7d271d9c17612ecd1a2d267e1c0e2bae05b3
Lehman Black <lblack@curtmfg.com>
2016-12-23 08:33:06 -0600

    Add MongoDB session in repository

--- repositories/MongoCustomerRepository.go
+++ repositories/MongoCustomerRepository.go
@@ -2,9 +2,11 @@ package repositories
 
 import (
 	"github.com/WileESpaghetti/curt-db-utils/models"
+	"gopkg.in/mgo.v2"
 )
 
 type MongoCustomerRepository struct {
+	session *mgo.Session
 }
 
 func (repo MongoCustomerRepository) getByApiKey(apiKey) models.Customer {
