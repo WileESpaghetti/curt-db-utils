6707352556c2f5ac9590bb571966565d562b5ba4
Lehman Black <lblack@curtmfg.com>
2016-12-23 18:27:51 -0600

    Get CustomerUser by email address Test needs some cleanup. Everything is hard coded, and there is some setup/teardown logic that can be refactored out.

--- repositories/SqlCustomerUserRepository.go
+++ repositories/SqlCustomerUserRepository.go
@@ -14,7 +14,21 @@ func (repo SqlCustomerRepository) getByApiKey(apiKey string) models.CustomerUser
 	return nil
 }
 
-func (repo SqlCustomerRepository) getByEmail(email string) models.CustomerUser {
-	// FIXME not implemented
-	return nil
+func (repo SqlCustomerRepository) getByEmail(email string) (c models.CustomerUser, err error) {
+	customerUserFromEmail := `select customer.id, customer.email
+				from CustomerUser as customer
+				where UPPER(customer.email) = UPPER(?)
+				`
+
+	stmt, err := repo.session.Prepare(customerUserFromEmail)
+	if err != nil {
+		return c, err
+	}
+	defer stmt.Close()
+
+	customerUser := models.CustomerUser{"", ""}
+	result := stmt.QueryRow(email)
+	err = result.Scan(&customerUser.Id, &customerUser.Email)
+
+	return customerUser, err
 }
---
+++ repositories/SqlCustomerUserRepository_test.go
@@ -0,0 +1,58 @@
+package repositories
+
+import (
+	"database/sql"
+	"testing"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestGetByEmail(t *testing.T) {
+	var err error
+	testDb := "curt_db_utils_test"
+	customerUserTable := "CustomerUser"
+	session, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		fmt.Println(err)
+		t.Error("Could not connect to test database server")
+		return
+	}
+	defer session.Close()
+
+	_,err = session.Exec("DROP DATABASE IF EXISTS " + testDb)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_,err = session.Exec("CREATE DATABASE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("USE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("CREATE TABLE " + customerUserTable + "(id integer, email varchar(255))")
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("INSERT INTO " + customerUserTable + " (id, email) VALUES (1, 'example@example.com')")
+	if err != nil {
+		panic(err)
+	}
+
+	repo := SqlCustomerRepository{session: session}
+	customerUser, err := repo.getByEmail("example@example.com")
+	if ("1" != customerUser.Id) {
+		t.Error("Expected CustomerUser.Id to be\n expected: %s\nactual:  %s", "1", customerUser.Id)
+	}
+
+	_,err = session.Exec("DROP DATABASE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+}
+
