6a617b24c76d602bc846627e453f258b31cc4e18
Lehman Black <lblack@curtmfg.com>
2017-01-05 15:07:26 -0600

    Add MySQL version of GetByApiKey

--- repositories/SqlCustomerUserRepository.go
+++ repositories/SqlCustomerUserRepository.go
@@ -3,15 +3,42 @@ package repositories
 import (
 	"github.com/WileESpaghetti/curt-db-utils/models"
 	"database/sql"
+	"fmt"
 )
 
 type SqlCustomerRepository struct {
 	Session *sql.DB
 }
 
-func (repo SqlCustomerRepository) GetByApiKey(apiKey string) (c models.CustomerUser, err error) {
-	// FIXME not implemented
-	return models.CustomerUser{}, nil
+func (repo SqlCustomerRepository) GetByApiKey(apiKey string, apiKeyType string) (c models.CustomerUser, err error) {
+	customerUserFromKey := `select
+		cu.id,
+		cu.email
+	from CustomerUser as cu
+	join ApiKey as ak
+		on cu.id = ak.user_id
+	join ApiKeyType as akt
+		on ak.type_id = akt.id
+	where
+		UPPER(akt.type) = ? &&
+		UPPER(ak.api_key) = UPPER(?)
+	limit 1
+	`
+	stmt, err := repo.Session.Prepare(customerUserFromKey)
+	if err != nil {
+		return c, err
+	}
+	defer stmt.Close()
+
+	customerUser := models.CustomerUser{}
+	result := stmt.QueryRow(apiKeyType, apiKey)
+	err = result.Scan(&customerUser.Id, &customerUser.Email)
+
+	if err != nil {
+		err = fmt.Errorf("error: %s", "API Key or User does not exist")
+	}
+
+	return customerUser, err
 }
 
 func (repo SqlCustomerRepository) GetByEmail(email string) (c models.CustomerUser, err error) {
--- repositories/SqlCustomerUserRepository_test.go
+++ repositories/SqlCustomerUserRepository_test.go
@@ -56,3 +56,95 @@ func TestGetByEmail(t *testing.T) {
 	}
 }
 
+func TestGetByApiKey(t *testing.T) {
+	var err error
+	testDb := "curt_db_utils_test"
+	customerUserTable := "CustomerUser"
+	apiKeyTable := "ApiKey"
+	apiKeyTypeTable := "ApiKeyType"
+	session, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		fmt.Println(err)
+		t.Error("Could not connect to test database server")
+		return
+	}
+	defer session.Close()
+
+	_,err = session.Exec("DROP DATABASE IF EXISTS " + testDb)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_,err = session.Exec("CREATE DATABASE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("USE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("CREATE TABLE " + customerUserTable + "(id int, email varchar(255))")
+	if err != nil {
+		panic(err)
+	}
+
+	_,err = session.Exec("INSERT INTO " + customerUserTable + " (id, email) VALUES ('1', 'example@example.com')")
+	if err != nil {
+		panic(err)
+	}
+
+	// Add test API key
+	testCustomerUserId := "1"
+	testApiKeyTypeType := "TESTING_KEY"
+	testApiKeyTypeId := "1"
+	testApiKeyApiKey := "VALID_TEST_KEY"
+	testInvalidApiKeyApiKey := "INVALID_TEST_KEY"
+	testApiKeyId := "1"
+
+	createApiKeyTypeTable := fmt.Sprintf("CREATE TABLE %s (id integer, type varchar(255))", apiKeyTypeTable)
+	_,err = session.Exec(createApiKeyTypeTable)
+	if err != nil {
+		panic(err)
+	}
+
+	insertTestApiKeyType := fmt.Sprintf("INSERT INTO %s (id, type) VALUES (%s, '%s')", apiKeyTypeTable, testApiKeyTypeId, testApiKeyTypeType)
+	_,err = session.Exec(insertTestApiKeyType)
+	if err != nil {
+		panic(err)
+	}
+
+	createApiKeyTable := fmt.Sprintf("CREATE TABLE %s (id integer, user_id integer, type_id integer, api_key varchar(255))", apiKeyTable)
+	_,err = session.Exec(createApiKeyTable)
+	if err != nil {
+		panic(err)
+	}
+
+	insertTestApiKey := fmt.Sprintf("INSERT INTO %s (id, user_id, type_id, api_key) VALUES (%s, %s, %s, '%s')",
+		apiKeyTable, testApiKeyId, testCustomerUserId, testApiKeyTypeId, testApiKeyApiKey)
+	_,err = session.Exec(insertTestApiKey)
+	if err != nil {
+		panic(err)
+	}
+
+	// Test the result
+	repo := SqlCustomerRepository{Session: session}
+	customerUser, err := repo.GetByApiKey(testApiKeyApiKey, testApiKeyTypeType)
+	if (fmt.Sprintf(customerUser.Id) != testCustomerUserId) {
+		t.Errorf("Expected CustomerUser.Id to be\n expected: %s\nactual:  %s", testCustomerUserId, customerUser.Id)
+	}
+	if (err != nil) {
+		t.Error("Unexpected error occured from GetUserByApiKey")
+	}
+
+	_, err = repo.GetByApiKey(testInvalidApiKeyApiKey, testApiKeyTypeType)
+	if (err == nil) {
+		t.Error("Error not thrown for invalid API key")
+	}
+
+	_,err = session.Exec("DROP DATABASE " + testDb)
+	if err != nil {
+		panic(err)
+	}
+}
