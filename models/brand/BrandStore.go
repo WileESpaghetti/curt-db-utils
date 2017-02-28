629fdcd6dd07bb1a61d7a71d9c39974fbfcbc4ff
Lehman Black <lblack@curtmfg.com>
2017-02-27 23:59:12 -0600

    Add godoc

--- models/brand/BrandStore.go
+++ models/brand/BrandStore.go
@@ -2,13 +2,16 @@ package brand
 
 // TODO Eventually it would be nice if this could implement from a Store interface
 type BrandStore struct {
+	// Data access interface
 	Repository BrandRepository
 }
 
+// Retrieve a stored Brand
 func (store BrandStore) Get(id int) (brand Brand, err error) {
 	return store.Repository.GetById(id)
 }
 
+// Retrieve all available Brands
 func (store BrandStore) All() (brand []Brand, err error) {
 	return store.Repository.All()
 }
