c2f22d617d99d60f675b40da3db31ac2ea9a6194
Lehman Black <lblack@curtmfg.com>
2017-02-27 23:58:47 -0600

    Add godoc

--- models/brand/BrandLocator.go
+++ models/brand/BrandLocator.go
@@ -2,6 +2,9 @@ package brand
 
 // Placeholder until we can get "*Store" classes to inherit from a more generic Store type
 type BrandLocator interface {
+	//Add(brand Brand) (err error)
 	Get(id int) (brand Brand, err error)
+	//Update(brand Brand) (err error)
+	//Remove(brand Brand) (err error)
 	All() (brand []Brand, err error)
 }
